6c9528ffd50fdf15c3a9555cafa5d9a079b5c14e
Vladimir Eric <v.eric@snapaddy.com>
2021-08-27 08:37:44 +0200

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastButton, ToastController, ToastOptions } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
+
+  async showToast(): Promise<void> {
+    let options: ToastOptions = {
+      duration: 50000,
+      message: 'This is a very long message that has a lot of it written on it and repeated. This is a very long message that has a lot of it written on it and repeated. This is a very long message that has a lot of it written on it and repeated.'
+    };
+
+    // display error details button
+    this.addErrorDetailsButton(options);
+
+    // display close button
+    const closeButton: ToastButton = {
+      icon: 'close-outline',
+      role: 'cancel',
+      side: 'end',
+      handler: () => console.log('toast dismissed')
+    };
+    options.buttons.push(closeButton);
+
+    const toast = await this.toastController.create(options);
+
+    // show toast
+    await toast.present();
+  }
+
+  private async addErrorDetailsButton(opts: ToastOptions): Promise<void> {
+    const toastClass = 'my-error-toast';
+
+    if (opts.cssClass) {
+      Array.isArray(opts.cssClass) ? opts.cssClass.push(toastClass) : (opts.cssClass = [opts.cssClass, toastClass]);
+    } else {
+      opts.cssClass = toastClass;
+    }
+
+    const errorDetailsButton: ToastButton = {
+      side: 'end',
+      text: 'More details',
+      handler: () => console.log('Clicked on more details')
+    };
+    opts.buttons ? opts.buttons.push(errorDetailsButton) : (opts.buttons = [errorDetailsButton]);
+  }
 
 }
